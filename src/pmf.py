f8975fe8d6c81394ce4a2296eae6ba4c13254f3c
shockleylee <shockleylee@gmail.com>
2016-04-29 20:02:30 -0400

    ds

--- src/pmf.py
+++ src/pmf.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.INFO)
 class PMF(object):
     """Probabilistic Matrix Factorization model using pymc3."""
 
-    def __init__(self, train, dim, alpha=2, std=0.01, bounds=(-10, 10)):
+    def __init__(self, train, dim, alpha=2, std=0.01, bounds=(1, 5)):
         """Build the Probabilistic Matrix Factorization model using pymc3.
 
         :param np.ndarray train: The training data to use for learning the model.
@@ -26,7 +26,6 @@ class PMF(object):
         :param float std: Amount of noise to use for model initialization.
         :param (tuple of int) bounds: (lower, upper) bound of ratings.
             These bounds will simply be used to cap the estimates produced for R.
-
         """
         self.dim = dim
         self.alpha = alpha
@@ -62,3 +61,231 @@ class PMF(object):
 
     def __str__(self):
         return self.name
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+
+# First define functions to save our MAP estimate after it is found.
+# We adapt these from `pymc3`'s `backends` module, where the original
+# code is used to save the traces from MCMC samples.
+def save_np_vars(vars, savedir):
+    """Save a dictionary of numpy variables to `savedir`. We assume
+    the directory does not exist; an OSError will be raised if it does.
+    """
+    logging.info('writing numpy vars to directory: %s' % savedir)
+    os.mkdir(savedir)
+    shapes = {}
+    for varname in vars:
+        data = vars[varname]
+        var_file = os.path.join(savedir, varname + '.txt')
+        np.savetxt(var_file, data.reshape(-1, data.size))
+        shapes[varname] = data.shape
+
+        ## Store shape information for reloading.
+        shape_file = os.path.join(savedir, 'shapes.json')
+        with open(shape_file, 'w') as sfh:
+            json.dump(shapes, sfh)
+            
+            
+def load_np_vars(savedir):
+    """Load numpy variables saved with `save_np_vars`."""
+    shape_file = os.path.join(savedir, 'shapes.json')
+    with open(shape_file, 'r') as sfh:
+        shapes = json.load(sfh)
+
+    vars = {}
+    for varname, shape in shapes.items():
+        var_file = os.path.join(savedir, varname + '.txt')
+        vars[varname] = np.loadtxt(var_file).reshape(shape)
+        
+    return vars
+
+
+# Now define the MAP estimation infrastructure.
+def _map_dir(self):
+    basename = 'pmf-map-d%d' % self.dim
+    return os.path.join('data', basename)
+def _means_dir(self):
+    basename = 'pmf-mean-d%d' % self.dim
+    return os.path.join('data', basename)
+def _sds_dir(self):
+    basename = 'pmf-sd-d%d' % self.dim
+    return os.path.join('data', basename)
+def _elbos_dir(self):
+    basename = 'pmf-elbo-d%d' % self.dim
+    return os.path.join('data', basename)
+
+def _find_map(self):
+    """Find mode of posterior using Powell optimization."""
+    tstart = time.time()
+    with self.model:
+        logging.info('finding PMF MAP using Powell optimization...')
+        self._map = pm.find_MAP(fmin=sp.optimize.fmin_powell, disp=True)
+
+    elapsed = int(time.time() - tstart)
+    logging.info('found PMF MAP in %d seconds' % elapsed)
+    
+    # This is going to take a good deal of time to find, so let's save it.
+    save_np_vars(self._map, self.map_dir)
+    
+def _load_map(self):
+    self._map = load_np_vars(self.map_dir)
+def _load_means(self):
+    self._means = load_np_vars(self.means_dir)
+def _load_sds(self):
+    self._sds = load_np_vars(self.sds_dir)
+def _load_elbos(self):
+    self._elbos = load_np_vars(self.elbos_dir)
+
+def _map(self):
+    try:
+        return self._map
+    except:
+        if os.path.isdir(self.map_dir):
+            self.load_map()
+        else:
+            self.find_map()
+        return self._map
+
+def _means(self):
+    try:
+        return self._means
+    except:
+        if os.path.isdir(self.advi_dir):
+            self.load_advi()
+        else:
+            self.find_advi()
+        return self._means
+def _sds(self):
+    try:
+        return self._sds
+    except:
+        if os.path.isdir(self.advi_dir):
+            self.load_advi()
+        else:
+            self.find_advi()
+        return self._sds
+def _elbos(self):
+    try:
+        return self._elbos
+    except:
+        if os.path.isdir(self.advi_dir):
+            self.load_elbos()
+        else:
+            self.find_advi()
+        return self._elbos
+
+def eval_advi(pmf_model, train, test):
+    U = pmf_model.means['U']
+    V = pmf_model.means['V']
+    # Make predictions and calculate RMSE on train & test sets.
+    predictions = pmf_model.predict(U, V)
+    train_rmse = rmse(train, predictions)
+    test_rmse = rmse(test, predictions)
+    overfit = test_rmse - train_rmse
+    # Print report.
+    print 'PMF MAP training RMSE: %.5f' % train_rmse
+    print 'PMF MAP testing RMSE:  %.5f' % test_rmse
+    print 'Train/test difference: %.5f' % overfit
+    return test_rmse
+
+def eval_map(pmf_model, train, test):
+    U = pmf_model.map['U']
+    V = pmf_model.map['V']
+    # Make predictions and calculate RMSE on train & test sets.
+    predictions = pmf_model.predict(U, V)
+    train_rmse = rmse(train, predictions)
+    test_rmse = rmse(test, predictions)
+    overfit = test_rmse - train_rmse
+    # Print report.
+    print 'PMF MAP training RMSE: %.5f' % train_rmse
+    print 'PMF MAP testing RMSE:  %.5f' % test_rmse
+    print 'Train/test difference: %.5f' % overfit
+    return test_rmse
+
+
+def _find_advi(self):
+    """Find mode of posterior using Powell optimization."""
+    tstart = time.time()
+    with self.model:
+        logging.info('finding PMF MAP using Powell optimization...')
+        #self._map = pm.find_MAP(fmin=sp.optimize.fmin_powell, disp=True)
+        self._means, self._sds, self._elbos = pm.variational.advi(model=self.model, n=20000, accurate_elbo=True)
+
+    elapsed = int(time.time() - tstart)
+    logging.info('found PMF MAP in %d seconds' % elapsed)
+    
+    # This is going to take a good deal of time to find, so let's save it.
+    save_np_vars(self._means, self.means_dir)
+    save_np_vars(self._sds, self.sds_dir)
+    save_np_vars(self._elbos, self.elbos_dir)
+
+
+def _predict(self, U, V):
+    """Estimate R from the given values of U and V."""
+    R = np.dot(U, V.T)
+    n, m = R.shape
+    sample_R = np.array([
+        [np.random.normal(R[i,j], self.std) for j in xrange(m)]
+        for i in xrange(n)
+    ])
+
+    # bound ratings
+    low, high = self.bounds
+    sample_R[sample_R < low] = low
+    sample_R[sample_R > high] = high
+    return sample_R
+
+
+
+
+# Draw MCMC samples.
+def _trace_dir(self):
+    basename = 'pmf-mcmc-d%d' % self.dim
+    return os.path.join('data', basename)
+
+def _draw_samples(self, nsamples=1000, njobs=2):
+    # First make sure the trace_dir does not already exist.
+    if os.path.isdir(self.trace_dir):
+        raise OSError(
+            'trace directory %s already exists. Please move or delete.' % self.trace_dir)
+    start = self.map  # use our MAP as the starting point
+    with self.model:
+        logging.info('drawing %d samples using %d jobs' % (nsamples, njobs))
+        step = pm.NUTS(scaling=start)
+        backend = pm.backends.Text(self.trace_dir)
+        logging.info('backing up trace to directory: %s' % self.trace_dir)
+        self.trace = pm.sample(nsamples, step, start=start, njobs=njobs, trace=backend)
+        
+def _load_trace(self):
+    with self.model:
+        self.trace = pm.backends.text.load(self.trace_dir)
+
+PMF.predict = _predict
+
+# Add eval function to PMF class.
+PMF.eval_map = eval_map
+    
+# Update our class with the new MAP infrastructure.
+PMF.find_map = _find_map
+PMF.load_map = _load_map
+PMF.map_dir = property(_map_dir)
+PMF.map = property(_map)
+
+# Update our class with the sampling infrastructure.
+PMF.trace_dir = property(_trace_dir)
+PMF.draw_samples = _draw_samples
+PMF.load_trace = _load_trace
+PMF.predict = _predict
+
+
+PMF.find_advi = _find_advi
+PMF.means_dir = property(_means_dir)
+PMF.means = property(_means)
+PMF.sds_dir = property(_sds_dir)
+PMF.sds = property(_sds)
+PMF.elbos_dir = property(_elbos_dir)
+PMF.elbos = property(_elbos)
--- src/pymc_train.py
+++ src/pymc_train.py
@@ -1,13 +1,122 @@
 import pystan,pickle,matplotlib,warnings
 import sys,time,os
 import numpy as np
+import pandas,pmf
 from sklearn.datasets import *
 from scipy.sparse import coo_matrix
 
 
-raw_data='../data/ml1m_d.sparse'
-with warnings.catch_warnings():
-  warnings.simplefilter("ignore")
-  (x,y) = load_svmlight_file(raw_data, dtype=np.int32, zero_based=False)
-x=coo_matrix(x)
-#ratings = np.transpose(np.array([x.row,x.col,x.data]))
+
+
+# Define our evaluation function.
+def rmse(test_data, predicted):
+    """Calculate root mean squared error.
+    Ignoring missing values in the test data.
+    """
+    I = ~np.isnan(test_data)   # indicator for missing values
+    N = I.sum()                # number of non-missing values
+    sqerror = abs(test_data - predicted) ** 2  # squared error array
+    mse = sqerror[I].sum() / N                 # mean squared error
+    return np.sqrt(mse)                        # RMSE
+
+import hashlib
+
+
+# Define a function for splitting train/test data.
+def split_train_test(data, percent_test=10):
+    """Split the data into train/test sets.
+    :param int percent_test: Percentage of data to use for testing. Default 10.
+    """
+    n, m = data.shape             # # users, # jokes
+    N = n * m                     # # cells in matrix
+    test_size = N / percent_test  # use 10% of data as test set
+    train_size = N - test_size    # and remainder for training
+
+    # Prepare train/test ndarrays.
+    train = data.copy().values
+    test = np.ones(data.shape) * np.nan
+
+    # Draw random sample of training data to use for testing.
+    tosample = np.where(~np.isnan(train))       # ignore nan values in data
+    idx_pairs = zip(tosample[0], tosample[1])   # tuples of row/col index pairs
+    indices = np.arange(len(idx_pairs))         # indices of index pairs
+    sample = np.random.choice(indices, replace=False, size=test_size)
+
+    # Transfer random sample from train set to test set.
+    for idx in sample:
+        idx_pair = idx_pairs[idx]
+        test[idx_pair] = train[idx_pair]  # transfer to test set
+        train[idx_pair] = np.nan          # remove from train set
+
+    # Verify everything worked properly
+    assert(np.isnan(train).sum() == test_size)
+    assert(np.isnan(test).sum() == train_size)
+    
+    # Finally, hash the indices and save the train/test sets.
+    index_string = ''.join(map(str, np.sort(sample)))
+    name = hashlib.sha1(index_string).hexdigest()
+    savedir = os.path.join('data', name)
+    save_np_vars({'train': train, 'test': test}, savedir)
+    
+    # Return train set, test set, and unique hash of indices.
+    return train, test, name
+
+
+def load_train_test(name):
+    """Load the train/test sets."""
+    savedir = os.path.join('data', name)
+    vars = load_np_vars(savedir)
+    return vars['train'], vars['test']
+
+ 
+
+
+
+if __name__ == '__main__':
+	raw_data='../data/ml1m_d.sparse'
+	with warnings.catch_warnings():
+	  warnings.simplefilter("ignore")
+	  (x,y) = load_svmlight_file(raw_data, dtype=np.int32, zero_based=False)
+	#x=coo_matrix(x)
+	data = pandas.DataFrame(data=x.todense())
+	train, test, name_hash = split_train_test(data, percent_test=10)
+
+	#ratings = np.transpose(np.array([x.row,x.col,x.data]))
+	# train, test, name = split_train_test(data)
+
+	#train, test = load_train_test('6bb8d06c69c0666e6da14c094d4320d115f1ffc8')
+	# Let's see the results:
+	baselines = {}
+	for name in baseline_methods:
+	    Method = baseline_methods[name]
+	    method = Method(train)
+	    baselines[name] = method.rmse(test)
+	    print '%s RMSE:\t%.5f' % (method, baselines[name])
+
+	# We use a fixed precision for the likelihood.
+	# This reflects uncertainty in the dot product.
+	# We choose 2 in the footsteps Salakhutdinov
+	# Mnihof.
+	ALPHA = 2
+
+	# The dimensionality D; the number of latent factors.
+	# We can adjust this higher to try to capture more subtle
+	# characteristics of each joke. However, the higher it is,
+	# the more expensive our inference procedures will be.
+	# Specifically, we have D(N + M) latent variables. For our
+	# Jester dataset, this means we have D(1100), so for 5
+	# dimensions, we are sampling 5500 latent variables.
+	DIM = 10
+
+
+	pmf = PMF(train, DIM, ALPHA, std=0.01)
+	# Find MAP for PMF.
+	#pmf.find_map()
+	pmf.find_advi()
+	# pmf.load_map()
+	
+	# Evaluate PMF MAP estimates.
+	#pmf_rmse = pmf.eval_map(train, test)
+	pmf_rmse = pmf.eval_advi(train, test)
+	pmf_improvement = baselines['mom'] - pmf_rmse
+	print 'PMF Improvement:   %.5f' % pmf_improvement
